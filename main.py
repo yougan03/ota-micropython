96e6268ad3d8feec0551d504976296fc3679ce36
Yougan bhalaji <youganbhalaji@gmail.com>
2025-05-16 13:28:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# import ugit
-# from machine import Pin
-# import time
-
-# pin = Pin(0,Pin.IN,Pin.PULL_UP)
-# if pin.value() is 0:
-#     ugit.pull_all()
-    
-# #main code here
-# TIME_MS=100
-# LED = Pin("LED", Pin.OUT)
-# while True:
-#     LED.off()
-#     time.sleep_ms(TIME_MS)
-#     LED.on()
-#     time.sleep_ms(TIME_MS)
-
-# uart tx and rx
-from machine import UART, Pin
+import ugit
+from machine import Pin
 import time
 
-uart1 = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
-
-uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
-
-txdat= 'world\n\r'
-
-uart1.write(txdat)
-time.sleep(2)
-rxdat=bytes()
-
-while uart0.any() > 0:
-    rxdat += uart0.read(1)
+pin = Pin(0,Pin.IN,Pin.PULL_UP)
+if pin.value() is 0:
+    ugit.pull_all()
     
-    print(rxdat.decode('utf-8'))
+#main code here
+TIME_MS=100
+LED = Pin("LED", Pin.OUT)
+while True:
+    LED.off()
+    time.sleep_ms(TIME_MS)
+    LED.on()
+    time.sleep_ms(TIME_MS)
+
+# uart tx and rx
+# from machine import UART, Pin
+# import time
+
+# uart1 = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
+
+# uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+# txdat= 'world\n\r'
+
+# uart1.write(txdat)
+# time.sleep(2)
+# rxdat=bytes()
+
+# while uart0.any() > 0:
+#     rxdat += uart0.read(1)
+    
+#     print(rxdat.decode('utf-8'))
 
